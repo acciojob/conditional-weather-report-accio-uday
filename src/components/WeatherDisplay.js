e0a783d97ae29c5e1fb86e97ef59ecc47a5bb623
accio-uday <udaymanideep2002@gmail.com>
2025-08-13 21:30:41 +0530

    Create WeatherDisplay.js

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function WeatherDisplay({ weather }) {
+  const color = weather.temperature > 20 ? "red" : "blue";
+
+  return (
+    <div>
+    <p>
+    Temperature: <span style={{ color }}>{weather.temperature}</span>
+    </p>
+  <p>
+    Conditions: {weather.conditions}</p>
+    </p>
+    </div>
+  );
+}
+
+export default WeatherDisplay;
